5496e7ead82088ed8f8bdb62443823b6e1077701
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-03-01 21:23:52 +0100

    add an interprter to resolve the syntax tree

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from interpreter import Interpreter
 from lexer import Lexer
 from parser_ import Parser
+from interpreter import Interpreter
 
 while True:
-    text = input("clac >")
-    lexer = Lexer(text)
-    tokens = lexer.generate_tokens()
-    parser = Parser(tokens)
-    tree = parser.parse()
-    print(tree)
+    try:
+        text = input("clac >")
+        lexer = Lexer(text)
+        tokens = lexer.generate_tokens()
+        parser = Parser(tokens)
+        tree = parser.parse()
+
+        if not tree: continue
+
+        interpreter = Interpreter()
+        value = interpreter.visit(tree)
+        print(value)
+    except Exception as e:
+        print(e)
